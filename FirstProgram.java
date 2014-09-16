a9009515d43befab2b367362a5215096945cd476
Silverlink34 <brandongaliher7@gmail.com>
2014-09-15 23:30:21 -0400

    notes

---
+++ FirstProgram.java
@@ -0,0 +1,18 @@
+
+/**
+*This is a test program, my very first Java program.
+*I am very excite.
+*/
+public class FirstProgram {
+	public static void main(String[] args) {
+		String input;
+		System.out.print("Type something! Then press enter.");
+		System.out.println();
+		input = TextIO.getln();
+		System.out.println();
+		System.out.println();
+		System.out.println("You just typed the following string:");
+		System.out.println();
+		System.out.println(input);
+	} //End of Main()
+} //End of FirstProgram
